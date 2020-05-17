d91a57a88520cbdfb629a76d4bfbb099e43f8e83
Spencer James <sj@sourcetheorem.com>
2020-05-16 22:54:09 -0700

    Add Stats Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
 import { AllRunsViewComponent } from './all-runs-view/all-runs-view.component';
+import { AllRunsStatsComponent } from './all-runs-stats/all-runs-stats.component';
 
 const routes: Routes = [
   { path: 'runs-list', component: AllRunsViewComponent },
   { path: 'run/:id', component: ActivityDetailComponent },
+  { path: 'stats', component: AllRunsStatsComponent },
   { path: '', redirectTo: '/runs-list', pathMatch: 'full' },
 ];
 
